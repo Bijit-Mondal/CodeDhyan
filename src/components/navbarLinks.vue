e1396732c07172e430e45e80564a928f1355d4db
Asim Roy Chowdhury <asimrc2002@gmail.com>
2023-10-20 07:40:43 +0530

    added navbar links component

---
+++ src/components/navbarLinks.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="title" :style="{'font-size': fontSize}">
+        <a :href="link">{{ title }}</a>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            title: String,
+            fontSize: String,
+            link: String
+        }
+    }
+</script>
+
+<style scoped>
+    .title{
+        color: rgb(var(--text));
+        font-family: Josefin Sans;
+        
+    }
+</style>
