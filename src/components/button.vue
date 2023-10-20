5cffcedc0a7b7b4e798c34c2f9dd7606b900cdd2
Asim Roy Chowdhury <asimrc2002@gmail.com>
2023-10-20 07:39:41 +0530

    added custom button component

---
+++ src/components/button.vue
@@ -0,0 +1,26 @@
+<template>
+    <button class="button">
+        {{title}}
+    </button>
+</template>
+
+<script>
+export default {
+    props: {
+        title: String,
+    }
+}
+</script>
+
+<style scoped>
+.button {
+    padding: 0.5rem 2rem;
+    border-radius: 77px;
+    background: rgb(var(--vs-primary));
+    box-shadow: 2px 4px 4px 0px rgba(var(--vs-primary), 0.15);
+    border: 0;
+    color: rgb(var(--text));
+    font-family: Josefin Sans;
+    font-weight: 600;
+}
+</style>
