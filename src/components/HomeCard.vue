217804c321c8369d1dcbd779c69efc5264299cab
Asim Roy Chowdhury <asimrc2002@gmail.com>
2023-10-20 07:40:15 +0530

    added home cards design

---
+++ src/components/HomeCard.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="card">
+        <img :src="image" :alt="altTag" />
+        <div class="title">
+            {{ title }}
+        </div>
+        <div class="description">
+            {{ description }}
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            image: String,
+            altTag: String,
+            title: String,
+            description: String
+        }
+    }
+</script>
+
+<style scoped>
+    .card{
+        display: flex;
+        flex-direction: column;
+        width: max(31%,100px);
+        height: 100%;
+        padding: 20px;
+        align-items: center;
+        justify-content: center;
+        gap: 10px;
+    }
+    .title{
+        color: #fff;
+        text-align: center;
+        font-weight: 500;
+        font-size: x-large;
+        font-family: Josefin Sans;
+    }
+    .description{
+        color: #fff;
+        text-align: center;
+        font-weight: 300;
+        font-size: medium;
+        padding: 0px 40px;
+        font-family: Josefin Sans;
+    }
+</style>
