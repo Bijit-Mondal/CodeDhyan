5fd8e55223922b9e2087f70d79805889ee84a989
Asim Roy Chowdhury <asimrc2002@gmail.com>
2023-10-30 13:14:10 +0530

    Updated the theme slider

--- src/components/themeSlider.vue
+++ src/components/themeSlider.vue
@@ -1,62 +1,91 @@
 <template>
-    <div style="display: flex;">
+  <div style="display: flex;">
     <input type="checkbox" id="toggle" v-model="isChecked" @change="toggleTheme">
-    <label for="toggle" class="slider" onclick="slide()">
+    <label for="toggle" class="slider">
     </label>
-    </div>
+  </div>
 </template>
 
 <script>
+import { ref, onMounted } from 'vue';
+
 export default {
-  data() {
-    return {
-      isChecked: false,
+  setup() {
+    const isChecked = ref(false);
+    const userTheme = ref('light-theme');
+
+    const setTheme = (theme) => {
+      localStorage.setItem('user-theme', theme);
+      userTheme.value = theme;
+      document.documentElement.className = theme;
     };
-  },
-  methods: {
-    toggleTheme() {
-      document.body.classList.toggle("changeTheme", this.isChecked);
-    },
+
+    const toggleTheme = () => {
+      const activeTheme = localStorage.getItem('user-theme');
+      if (activeTheme === 'light-theme') {
+        setTheme('dark-theme');
+      } else {
+        setTheme('light-theme');
+      }
+    };
+
+    const getMediaPreference = () => {
+      const hasDarkPreference = window.matchMedia('(prefers-color-scheme: dark)').matches;
+      return hasDarkPreference ? 'dark-theme' : 'light-theme';
+    };
+
+    const getTheme = () => {
+      return localStorage.getItem('user-theme');
+    };
+
+    onMounted(() => {
+      const initUserTheme = getTheme() || getMediaPreference();
+      console.log(initUserTheme);
+      isChecked.value = initUserTheme === 'dark-theme';
+      setTheme(initUserTheme);
+    });
+
+    return { isChecked, toggleTheme };
   },
 };
 </script>
 
 <style scoped>
 .slider {
-    display: flex;
-    justify-content: center;
-    align-items: start;
-    padding: 0.2rem 0rem;
-    height: 2.5rem;
-    width: 1.2rem;
-    background: rgb(var(--vs-primary));
-    border-radius: 36px;
-    cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: start;
+  padding: 0.3rem 0rem;
+  height: 2.5rem;
+  width: 1.2rem;
+  background: rgb(var(--vs-primary));
+  border-radius: 36px;
+  cursor: pointer;
 }
 
 .slider::after {
-    content: "";
-    border-radius: 8rem;
-    background: rgb(var(--text));
-    width: 0.8rem;
-    height: 0.8rem;
-    transition: 0.3s;
+  content: "";
+  border-radius: 8rem;
+  background: rgb(var(--text));
+  width: 0.8rem;
+  height: 0.8rem;
+  transition: 0.3s;
 
 }
 
-input{
-    visibility: hidden;
+input {
+  visibility: hidden;
 }
 
-input:checked + .slider {
-    background: #919191;
-    align-items: end;
-    --bg: 38, 42, 56;
+input:checked+.slider {
+  background: #919191;
+  --bg: 38, 42, 56;
 }
 
-input:checked + .slider::after {
-    background: #000;
-    transition: 0.3s;
+input:checked+.slider::after {
+  background: #000;
+  transform: translateY(150%);
+  transition: 0.3s;
 
 }
 </style>
