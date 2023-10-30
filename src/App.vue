c04d5daad8a0a657b9a4cbafa2e2ae76ae126ecb
Asim Roy Chowdhury <asimrc2002@gmail.com>
2023-10-30 13:10:19 +0530

    added Side Navbar but not used

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 // import { RouterLink, RouterView } from 'vue-router'
 import NavBar from './components/NavBar.vue'
-import SideStickyNav from './components/SideStickyNav.vue';
+// import SideStickyNav from './components/SideStickyNav.vue';
 import FooterComponent from './components/FooterComponent.vue';
 </script>
 
---
+++ src/components/SideStickyNav.vue
@@ -0,0 +1,63 @@
+
+<template>
+    <div class="fixedDiv">
+        <table class="table">
+            <tbody>
+                <td>
+                    <NavBarLinks title="About" link="#" fontSize="1rem" :rotate="true" />
+                </td>
+                <td>
+                    <NavBarLinks title="Members" link="#" fontSize="1rem" :rotate="true" />
+                </td>
+                <td>
+                    <NavBarLinks title="Past Events" link="#" fontSize="1rem" :rotate="true" />
+                </td>
+                <td>
+                    <NavBarLinks title="Forum" link="#" fontSize="1rem" :rotate="true" />
+                </td>
+                <td>
+                    <NavBarLinks title="Announcement" link="#" fontSize="1rem" :rotate="true" />
+                </td>
+                <td>
+                </td>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+
+<script>
+import NavBarLinks from './NavBarLinks.vue';
+
+export default {
+    components: {
+        NavBarLinks,
+    }
+}
+
+</script>
+
+
+<style scoped>
+.fixedDiv {
+    display: flex;
+    flex-direction: column;
+    z-index: 10;
+    height: 80%;
+    width: auto;
+    position: fixed;
+    right: 0;
+    background-color: rgb(var(--vs-primary));
+    border-radius: 1rem 0rem 0rem 1rem;
+    justify-content: space-around;
+    align-items: center;
+}
+
+.table {
+    text-align: center;
+    height: 100%;
+    writing-mode: vertical-rl;
+    padding: 1rem;
+}
+
+</style>
